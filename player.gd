6f5b1dcfb2e0a70af9144c4bb40b8bd791ec9cbf
voxelv <timslippy@librem.one>
2021-08-15 10:46:13 -0700

    WIP

--- player.gd
+++ player.gd
@@ -1,22 +1,35 @@
 extends KinematicBody
 
-export var speed : float = 20
-export var air_speed : float = 20
-export var acceleration : float = 15
-export var air_acceleration : float = 5
+# Parameter exports
 export var gravity : float = 0.98
-export var max_terminal_velocity : float = 54
-export var jump_power : float = 20
-export var flap_power : float = 2
 
+# On Ground exports
+export var speed : float = 1
+export var acceleration : float = 1
+export var jump_power : float = 1
+
+# In Air exports
+export var thrust_power : float = 1
+export var drag_coef : float = 1
+export var lift_coef : float = 1
+export var mass : float = 1
+
+# Camera exports
 export(float, 0.1, 1.0) var mouse_sensitivity := 0.3
-#@export_range(-90.0, 0.0)
-var min_pitch := -50.0
+export(float, -90.0, 0.0) var min_pitch := -50.0
 export(float, 0.0, 90.0) var max_pitch := 50.0
 
+# On Ground
 var velocity : Vector3
 var y_velocity : float
 
+# In Air
+var lift := 0.0
+var drag := 0.0
+var thrust := 0.0
+var weight := 0.0
+
+# Camera
 var mouse_captured := false
 
 onready var camera_pivot := find_node("camera_pivot") as Spatial
@@ -38,7 +51,7 @@ func _process(delta):
 func _input(event):
 	if event is InputEventMouseMotion:
 		if mouse_captured:
-			rotation_degrees.y -= event.relative.x * mouse_sensitivity
+			camera_pivot.rotation_degrees.y -= event.relative.x * mouse_sensitivity
 			camera_pivot.rotation_degrees.x -= event.relative.y * mouse_sensitivity
 			camera_pivot.rotation_degrees.x = clamp(camera_pivot.rotation_degrees.x, min_pitch, max_pitch)
 
@@ -49,38 +62,65 @@ func handle_movement(delta):
 	var direction = Vector3()
 	
 	if Input.is_action_pressed("move_forward"):
-		direction -= transform.basis.z
+		direction -= camera_pivot.transform.basis.z
 	
 	if Input.is_action_pressed("move_backward"):
-		direction += transform.basis.z
+		direction += camera_pivot.transform.basis.z
 		
 	if Input.is_action_pressed("move_left"):
-		direction -= transform.basis.x
+		direction -= camera_pivot.transform.basis.x
 	
 	if Input.is_action_pressed("move_right"):
-		direction += transform.basis.x
+		direction += camera_pivot.transform.basis.x
 	
 	direction = direction.normalized()
 	
 	if is_on_floor():
-		velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
+		velocity = handle_on_ground_movement(direction, delta)
 	else:
-		velocity = velocity.linear_interpolate(direction * air_speed, air_acceleration * delta)
+		velocity = handle_in_air_movement(direction, delta)
 	
-	if is_on_floor():
-		y_velocity = -0.01
-	else:
-		y_velocity = clamp(y_velocity - gravity, -max_terminal_velocity, max_terminal_velocity)
-	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		y_velocity = jump_power
-	elif Input.is_action_just_pressed("jump"):
-		y_velocity = flap_power
-	
-	velocity.y = y_velocity
 	velocity = move_and_slide(velocity, Vector3.UP)
 	
 	if translation.y < -125.0:
 		var pos = get_parent().find_node("player_spawn").translation
 		translation = pos
 		velocity = Vector3.ZERO
+
+func handle_on_ground_movement(direction, delta):
+	var v = velocity
+	
+	v = v.linear_interpolate(direction * speed, acceleration * delta)
+	y_velocity = -0.01
+	
+	if Input.is_action_just_pressed("jump"):
+		y_velocity = jump_power
+		
+	v.y = y_velocity
+	
+	return(v)
+
+func handle_in_air_movement(direction, delta):
+	var v = velocity
+	var use_wings : bool = Input.is_action_pressed("jump")
+	
+	print("s: %f h: %f" % [-v.z, translation.y])
+	
+	var weight_force = mass * gravity
+	var lift_force = lift_coef * abs(v.z * v.z)
+	
+	var drag_force = drag_coef * abs(v.z * v.z)
+	var thrust_force = 0.0
+	if use_wings:
+		thrust_force = thrust_power
+	
+	# Gravity: v(t) = a*t + v0
+	var vert_accel = (lift_force - weight_force) / mass
+	v.y = vert_accel * delta + v.y
+	
+	# Thrust/Drag
+	var trajectory_accel = (thrust_force - drag_force) / mass
+	v.z = -trajectory_accel * delta + v.z
+	
+	return(v)  
+
